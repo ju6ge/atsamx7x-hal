89056d5a973dbb171ec3966345c3c0bb9f8d2f65
ju6ge <judge@felixrichter.tech>
2020-04-07 22:06:39 +0200

    Initial release HAL

---
+++ README.md
@@ -0,0 +1,10 @@
+# ATSAMx7x Hardware Abstraction Layer
+
+This crate implementats traits from [embedded-hal] (https://crates.io/crates/embedded-hal) for the Atmel SAMEX7X architechture.
+
+It is still in the early stages of development, and is currently geared to run on an atsme70q21 cpu. But contributions are welcome.
+
+# OpenOCD
+
+The openocd configuration provided works for my board which is not an off the shelf board. So don't expected to just work for your platform.
+
---
+++ examples/blink_hello_world.rs
@@ -0,0 +1,74 @@
+#![no_std]
+#![no_main]
+#![feature(asm)]
+
+extern crate panic_halt;
+extern crate atsamx7x_hal as hal;
+
+use cortex_m::Peripherals;
+use cortex_m_rt::entry;
+use cortex_m_semihosting::{hprintln};
+
+use hal::target_device;
+use hal::gpio::*;
+use hal::clock_gen::{Clocks, MasterClockConfig, SlckConfig, MainckConfig, PllackConfig, UpllckConfig, SystemClockConfig, MasterDivider, MasterPrescale};
+use hal::serial::{config, Serial};
+use hal::time::*;
+use hal::delay::Delay;
+use embedded_hal::blocking::delay::{DelayMs, DelayUs};
+
+use embedded_hal::digital::v2::ToggleableOutputPin;
+
+use core::fmt::Write;
+
+#[entry]
+fn main() -> ! {
+	let cortex_p = cortex_m::Peripherals::take().unwrap();
+	let peripherals = target_device::Peripherals::take().unwrap();
+
+	let wdt = &peripherals.WDT;
+	wdt.wdt_mr.write( |w| w.wddis().set_bit() );
+
+	let mut pmc = peripherals.PMC;
+	let mut supc = peripherals.SUPC;
+
+	peripherals.EFC.eefc_fmr.write( |w| {
+		unsafe {w.fws().bits(5);}
+		w.cloe().set_bit();
+		w.scod().clear_bit()
+	});
+
+	let clocks:Clocks = SystemClockConfig{
+		slck_conf : SlckConfig::default(),
+		mainck_conf : MainckConfig::default().use_crystal(hal::time::MegaHertz(12).into()).disable_rc(),
+		plla_conf : PllackConfig::default().from_divider(1, 49).startup_cycles(100),
+		upll_conf : UpllckConfig::default().enable(),
+		mck_conf :MasterClockConfig::default().src_pllack().from_divider(MasterPrescale::Pres2, MasterDivider::Div2)
+	}.freeze(&mut pmc, &mut supc);
+
+	let mut delay = Delay::new(cortex_p.SYST, &clocks);
+
+	let pioc = peripherals.PIOC.split(&mut pmc);
+	let mut pin0 = pioc.p19.into_open_drain_output();
+	let mut pin1 = pioc.p10.into_open_drain_output();
+
+	let pioa = peripherals.PIOA.split(&mut pmc);
+	let tx = pioa.p10.into_pmd0();
+	let rx = pioa.p9.into_pmd0();
+
+	let mut serial = Serial::uart0(
+		peripherals.UART0,
+		(tx, rx),
+		config::UartConfig::default().baudrate(115_200.bps()),
+		&clocks,
+		&mut pmc
+	).unwrap();
+
+	//blink
+	loop {
+		pin0.toggle().unwrap();
+		pin1.toggle().unwrap();
+		delay.delay_ms(500 as u32);
+		writeln!(serial, "Hello, world!\r").unwrap();
+	}
+}
