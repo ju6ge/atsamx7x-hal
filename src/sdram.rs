cbe0597ea3b67e63b9063f5665d39ed79c7bb593
ju6ge <judge@felixrichter.tech>
2020-05-09 14:27:20 +0200

    cleanup of complexity and added some comments

--- src/sdram.rs
+++ src/sdram.rs
@@ -1,8 +1,9 @@
-use crate::gpio::pioa::{PA18, PA20, PA0, PA15, PA16};
-use crate::gpio::pioc::{PC0, PC1, PC2, PC3, PC4, PC5, PC6, PC7, PC18, PC15, PC20, PC21, PC22, PC23, PC24, PC25, PC26, PC27, PC28, PC29, PC31};
-use crate::gpio::piod::{PD13, PD14, PD15, PD16, PD17, PD23, PD29};
-use crate::gpio::pioe::{PE0, PE1, PE2, PE3, PE4, PE5};
-use crate::gpio::{PeripheralCntr, PeriphA, PeriphC};
+//! SDRAM controller setup
+
+// To see an example of how to use this abstraction take a look at:
+//
+// https://github.com/ju6ge/hd_embedded_rust
+
 use crate::time::{PicoSeconds};
 use crate::clock_gen::Clocks;
 use crate::delay::Delay;
@@ -12,7 +13,6 @@ use embedded_hal::blocking::delay::{DelayUs};
 
 use target_device::sdramc::sdramc_mr::MODE_A as SdramMode;
 use cortex_m::asm;
-
 
 pub enum SdramColumns {
 	Columns256,
@@ -95,154 +95,8 @@ pub struct SdramConfig {
 #[derive(Debug)]
 pub struct InvalidConfig;
 
+/// Trait to for implement for Pin Configuration
 pub trait SdramPins {}
-
-pub trait Addr0 {}
-pub trait Addr1 {}
-pub trait Addr2 {}
-pub trait Addr3 {}
-pub trait Addr4 {}
-pub trait Addr5 {}
-pub trait Addr6 {}
-pub trait Addr7 {}
-pub trait Addr8 {}
-pub trait Addr9 {}
-pub trait Addr10 {}
-pub trait Addr11 {}
-pub trait Addr12 {}
-pub trait Data0 {}
-pub trait Data1 {}
-pub trait Data2 {}
-pub trait Data3 {}
-pub trait Data4 {}
-pub trait Data5 {}
-pub trait Data6 {}
-pub trait Data7 {}
-pub trait Data8 {}
-pub trait Data9 {}
-pub trait Data10 {}
-pub trait Data11 {}
-pub trait Data12 {}
-pub trait Data13 {}
-pub trait Data14 {}
-pub trait Data15 {}
-pub trait Bank0{}
-pub trait Bank1{}
-pub trait NBS0 {}
-pub trait NBS1 {}
-pub trait CK {}
-pub trait CKE {}
-pub trait CS {}
-pub trait RAS {}
-pub trait CAS {}
-pub trait WE {}
-
-
-macro_rules! sdram_pins {
-	( $($pin:ident: [$($inst:ty), *])+ ) => {
-		$(
-			$(
-				impl $pin for $inst {}
-			)*
-		)+
-	}
-}
-
-sdram_pins! {
-	Addr0  : [ PC20<PeripheralCntr<PeriphA>> ]
-	Addr1  : [ PC21<PeripheralCntr<PeriphA>> ]
-	Addr2  : [ PC22<PeripheralCntr<PeriphA>> ]
-	Addr3  : [ PC23<PeripheralCntr<PeriphA>> ]
-	Addr4  : [ PC24<PeripheralCntr<PeriphA>> ]
-	Addr5  : [ PC25<PeripheralCntr<PeriphA>> ]
-	Addr6  : [ PC26<PeripheralCntr<PeriphA>> ]
-	Addr7  : [ PC27<PeripheralCntr<PeriphA>> ]
-	Addr8  : [ PC28<PeripheralCntr<PeriphA>> ]
-	Addr9  : [ PC29<PeripheralCntr<PeriphA>> ]
-	Addr10 : [ PD13<PeripheralCntr<PeriphC>> ]
-	Addr11 : [ PC31<PeripheralCntr<PeriphA>> ]
-	Addr12 : [ PA18<PeripheralCntr<PeriphC>> ]
-
-	Data0  : [ PC0<PeripheralCntr<PeriphA>> ]
-	Data1  : [ PC1<PeripheralCntr<PeriphA>> ]
-	Data2  : [ PC2<PeripheralCntr<PeriphA>> ]
-	Data3  : [ PC3<PeripheralCntr<PeriphA>> ]
-	Data4  : [ PC4<PeripheralCntr<PeriphA>> ]
-	Data5  : [ PC5<PeripheralCntr<PeriphA>> ]
-	Data6  : [ PC6<PeripheralCntr<PeriphA>> ]
-	Data7  : [ PC7<PeripheralCntr<PeriphA>> ]
-	Data8  : [ PE0<PeripheralCntr<PeriphA>> ]
-	Data9  : [ PE1<PeripheralCntr<PeriphA>> ]
-	Data10 : [ PE2<PeripheralCntr<PeriphA>> ]
-	Data11 : [ PE3<PeripheralCntr<PeriphA>> ]
-	Data12 : [ PE4<PeripheralCntr<PeriphA>> ]
-	Data13 : [ PE5<PeripheralCntr<PeriphA>> ]
-	Data14 : [ PA15<PeripheralCntr<PeriphA>> ]
-	Data15 : [ PA16<PeripheralCntr<PeriphA>> ]
-
-	Bank0  : [ PA20<PeripheralCntr<PeriphC>> ]
-	Bank1  : [ PA0<PeripheralCntr<PeriphC>> ]
-
-	NBS0   : [ PC18<PeripheralCntr<PeriphA>> ]
-	NBS1   : [ PD15<PeripheralCntr<PeriphC>> ]
-
-	CK     : [ PD23<PeripheralCntr<PeriphC>> ]
-	CKE    : [ PD14<PeripheralCntr<PeriphC>> ]
-	CS     : [ PC15<PeripheralCntr<PeriphA>> ]
-	RAS    : [ PD16<PeripheralCntr<PeriphC>> ]
-	CAS    : [ PD17<PeripheralCntr<PeriphC>> ]
-	WE     : [ PD29<PeripheralCntr<PeriphC>> ]
-}
-
-impl<A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12,
-	D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12,
-	D13, D14, D15, B0, B1, N0, N1, CKT, CKET, CST, RAST, CAST, WET> SdramPins
-
-	for (A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12,
-		D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12,
-		D13, D14, D15, B0, B1, N0, N1, CKT, CKET, CST, RAST, CAST, WET)
-
-	 where
-		A0 : Addr0,
-		A1 : Addr1,
-		A2 : Addr2,
-		A3 : Addr3,
-		A4 : Addr4,
-		A5 : Addr5,
-		A6 : Addr6,
-		A7 : Addr7,
-		A8 : Addr8,
-		A9 : Addr9,
-		A10 : Addr10,
-		A11 : Addr11,
-		A12 : Addr12,
-		D0 : Data0,
-		D1 : Data1,
-		D2 : Data2,
-		D3 : Data3,
-		D4 : Data4,
-		D5 : Data5,
-		D6 : Data6,
-		D7 : Data7,
-		D8 : Data8,
-		D9 : Data9,
-		D10 : Data10,
-		D11 : Data11,
-		D12 : Data12,
-		D13 : Data13,
-		D14 : Data14,
-		D15 : Data15,
-		B0 : Bank0,
-		B1 : Bank1,
-		N0 : NBS0,
-		N1 : NBS1,
-		CKT : CK,
-		CKET : CKE,
-		CST : CS,
-		RAST : RAS,
-		CAST : CAS,
-		WET : WE
-	{}
 
 pub struct Sdram<PINS> {
 	sdramc : SDRAMC,
